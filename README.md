0158ae69762198295719232758651079e571de50
Kenichi Yamamoto <itskenichi@gmail.com>
2017-01-12 02:20:11 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# Tipper
-A simple tip calculator using HTML and PHP
+# Pre-work - *Tipper*
+
+**Tipper** is a tip calculator PHP page.
+
+Submitted by: **Kenichi Yamamoto**
+
+Time spent: **8** hours spent in total
+
+## User Stories
+
+The following **required** functionality is complete:
+* [x] User can enter a bill amount, choose a tip percentage, and submit the form to see the tip and total values.
+* [x] Tip percentage choices use a PHP loop to output three radio buttons.
+* [x] PHP code sets reasonable default values for the form.
+* [x] PHP code confirms the presence and correct format of submitted values.
+* [x] Page indicates any form errors which need to be fixed.
+* [x] Submitted form values are retained when errors or results are shown.
+
+The following **optional** features are implemented:
+* [x] Add support for custom tip percentage
+* [x] Add support for splitting the tip and total
+
+The following **additional** features are implemented:
+
+* [ ] List anything else that you can get done to improve the functionality!
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented user stories:
+
+<img src='http://imgur.com/a/oFT6s' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+GIF created with [LiceCap](http://www.cockos.com/licecap/).
+
+## Notes
+
+One of the challenges encountered when making the app was trying to get the values set by the user to remain the same after an error occurs. This was accomplished by using php variables to hold the values of the different fields.
+
+Another challenge was figuring out how to show/hide the sections that contained the total tip as well as the split tip. This was accomplished using booleans within the css of each div box to set the display to block or none in order to show or hide each section, respectively.
+
+## License
+
+    Copyright 2017 Kenichi Yamamoto
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ tipper.php
@@ -0,0 +1,244 @@
+<?php
+	// Initialize the variables used by the app
+	$custom = 5;
+	$bill = 0;
+	$tip_per = "";
+	$tip = "";
+	$total = "";
+	$errBill = false;
+	$errTip = false;
+	$errSplit = false;
+	$showTotal = false;
+	$showSplit = false;
+	$lastChoice = 0;
+	$numTippers = 1;
+	$splitTip = "";
+	$splitTotal = "";
+	
+	function computeTip(){
+		// The global keyboard allows us to access the same variables declared outside of the function scope
+		// This prevents the function from creating its own local copy of the variable
+		global $custom;
+		global $bill;
+		global $tip_per;
+		global $tip;
+		global $total;
+		global $errBill;
+		global $errTip;
+		global $errSplit;
+		global $showTotal;
+		global $showSplit;
+		global $lastChoice;
+		global $numTippers;
+		global $splitTip;
+		global $splitTotal;
+		
+		// Check to see if the value entered within the 'bill' field is non-numeric or <= 0. If it is, set the error bool to true and return.
+		if(!is_numeric($_POST['bill']) || $_POST['bill'] <= 0)
+		{
+			$errBill = true;
+			return;
+		}
+		
+		// Rounds the value stored in 'bill' to the nearest two decimal places
+		// Also stores the value of the selected radio button and every other text field
+		$bill = round($_POST['bill'],2);
+		$tip_per = $_POST['tip'];
+		$custom = $_POST['cust_per'];
+		$numTippers = $_POST['split'];
+		
+		switch($tip_per)	// Checks to see which radio button was selected
+		{
+			case 0.10:
+				$lastChoice = 0;	// Keeps track of the currently selected radio button in order to keep it selected after the form is submitted
+				break;
+			case 0.15:
+				$lastChoice = 1;
+				break;
+			case 0.20:
+				$lastChoice = 2;
+				break;
+			case "custom":
+				$tip_per = $custom/100;	// Takes the percent value stored in the "custom" textbox and converts it to decimal form
+				$lastChoice = 3;
+				break;
+			default:
+				$lastChoice = 0;
+		}
+		
+		if(!is_numeric($tip_per) || $tip_per <= 0)
+		{
+			$errTip = true;
+			return;
+		}
+		
+		if(!is_numeric($numTippers) || $numTippers < 1)
+		{
+			$errSplit = true;
+			return;
+		}
+		
+		// number_format is used to format the result in standard currency notation, i.e. 1,000.40 instead of 1000.4
+		$tip = number_format($bill*$tip_per,2);
+		$total = number_format($bill+$tip,2);
+		$showTotal = true;
+		
+		if($numTippers == 1)	// Only one tipper, so split section is not needed/shown
+			return;
+		else
+		{
+			$numTippers = floor($numTippers);	// Rounds down to the nearest person
+			$splitTip = number_format($tip/$numTippers,2);
+			$splitTotal = number_format($total/$numTippers,2);
+			$showSplit = true;
+		}
+	}
+	
+	if(isset($_POST['bill']))	// Checks to see if the 'bill' field is set so that the function is not executed before the form elements are ready
+		computeTip();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Tip Calculator</title>
+<style type="text/css">
+<!-- All CSS formatting goes in here -->
+
+body {
+    background-color: lightblue;
+}
+
+h1 {
+    color: blue;
+    text-align: center;
+}
+
+#divMain {
+	text-align: center;
+    background-color: white;
+    width: 350px;
+    border: 5px solid blue;
+    padding: 15px;
+    margin: 0 auto;
+}
+
+#bill {
+	<?php
+		if($errBill == true){	// If this flag is set, set the label's formatting to be red and bolded, indicating that there is an error
+			echo "font-weight: bold;";
+			echo "color: red;";
+		}
+		else{
+			echo "font-weight: normal;";
+			echo "color: black;";
+		}
+	?>
+}
+
+#tip {
+	<?php
+		if($errTip == true){
+			echo "font-weight: bold;";
+			echo "color: red;";
+		}
+		else{
+			echo "font-weight: normal;";
+			echo "color: black;";
+		}
+	?>
+}
+
+#split {
+	<?php
+		if($errSplit == true){
+			echo "font-weight: bold;";
+			echo "color: red;";
+		}
+		else{
+			echo "font-weight: normal;";
+			echo "color: black;";
+		}
+	?>
+}
+
+#divTotal {
+	color: green;
+	font-weight: bold;
+	text-align: center;
+	border: 2px dashed green;
+    padding: 15px;
+	width:75%;
+	margin: 0 auto;
+	
+	<?php
+		if($showTotal == true)	// Shows the div normally if true, hides it if false
+			echo "display: block;";
+		else
+			echo "display: none;";
+	?>
+}
+
+#divSplit {
+	color: purple;
+	font-weight: bold;
+	text-align: center;
+	border: 2px dashed purple;
+    padding: 15px;
+	width:75%;
+	margin: 0 auto;
+	
+	<?php
+		if($showSplit == true)
+			echo "display: block;";
+		else
+			echo "display: none;";
+	?>
+}
+</style>
+</head>
+<body>
+
+<h1>Tipper</h1>
+
+<div id="divMain">
+	<form action="" method="post">
+		<label id="bill">Bill Subtotal: $</label> <input type="text" name="bill" value="<?php echo $bill ?>">
+		<br><br>
+		<label id="tip">Tip Percent: 
+		<?php
+			$tip_per = array("10", "15", "20");		//Store tip percentages in an array, in case we want to have more than 3 in the future.
+			// Creating radio buttons using a loop
+			for($i = 0; $i < count($tip_per); $i++) { 
+		?>
+			<input type="radio" name="tip" value="0.<?php echo $tip_per[$i]; ?>"
+			<?php if($i == $lastChoice) echo "checked"; ?>>
+			<?php echo $tip_per[$i]; ?>%
+		<?php
+			}
+		?>
+		<br>
+		<input type="radio" name="tip" value="custom" <?php if($lastChoice == 3) echo "checked"; ?>>
+		<label id="custom">Custom</label>
+		<input type="text" name="cust_per" value="<?php echo $custom ?>">%</label>
+		<br><br>
+		<label id="split">Split <input type="text" name="split" value="<?php echo $numTippers ?>"> person(s) </label>
+		<br><br>
+		<input type="submit" id="compute" value="Compute Tip">
+		<br><br>
+		<div id="divTotal">
+			Tip: $<?php echo $tip ?>
+			<br>
+			Total: $<?php echo $total ?>
+		</div>
+		<br>
+		<div id="divSplit">
+			Tip Each: $<?php echo $splitTip ?>
+			<br>
+			Total Each: $<?php echo $splitTotal ?>
+		</div>
+	</form>
+</div>
+
+</body>
+</html>
